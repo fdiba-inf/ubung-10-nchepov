5c43a93e7ba050495e8cf645c703527dde248365
nchepov <nedyalko.chepov@fdiba.tu-sofia.bg>
2020-12-07 18:48:26 +0000

    10

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -1,44 +1,57 @@
 package exercise10;
 
 public class Ellipse extends Figure {
+
     public Ellipse() {
+
         super(new Point(0, 0), 1, 1);
     }
 
-    public Ellipse(Point startPoint, double side2, double side1) {
-        super(startPoint, side2, side1);
+    public Ellipse(Point startPoint, double side1, double side2) {
+
+        super(new Point(startPoint), side1, side2);
     }
 
     public Ellipse(Ellipse otherEllipse) {
-        super(otherEllipse.startPoint, otherEllipse.side2, otherEllipse.side1);
-    } 
+        super(otherEllipse.startPoint, otherEllipse.side1, otherEllipse.side2);
+    }
 
     @Override
     public double calculatePerimeter() {
-        return Math.PI * (3.0 * (side2 + side1) - Math.sqrt((3.0 * side2 + side1) * (side2 + 3.0 * side1)));
+        return Math.PI * (3.0 * (side1 + side2) - Math.sqrt((3.0 * side1 + side2) * (side1 + 3.0 * side2)));
     }
 
     @Override
     public double calculateArea() {
-        return Math.PI * side2 * side1;
+        return Math.PI * side1 * side2;
     }
-    
+
     @Override
     public String getType() {
-         return (side2 == side1) ? "Circle" : "Ellipse";
+        return (side1 == side2) ? "Circle" : "Ellipse";
     }
 
     @Override
     public boolean equal(Figure otherFigure) {
-        if (otherFigure instanceof Ellipse) {
-            return super.equal(otherFigure);
-        } else {
-            return false;
-        }
+
+      if  (otherFigure instanceof Figure)  {
+        return super.equal(otherFigure);
+      }
+      else  {
+        return false;
+      }
+
     }
 
     public boolean containsClick(Point click) {
-        // Check if click point is inside the rectangle
+
+        double formula = (Math.pow((click.getX() - startPoint.getX()), 2)) / (side1*side1)
+        +
+        (Math.pow((click.getY() - startPoint.getY()), 2)) / (side2*side2);
+
+        if (formula <= 1) {
+          return true;
+        }
         return false;
     }
 }
